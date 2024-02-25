33da8cf1082ac10bd8fd6b7e46560f2fa1418542
Nevtech05 <nevtech254@gmail.com>
2024-02-25 13:41:19 +0300

    initial commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("Hello! please enter your name: ")
+age=int(input("Please enter your age: "))
+location=input("Where are you from? ")
+print(f"Welcome {name}! You are {age} years old and you are from {location}.")
